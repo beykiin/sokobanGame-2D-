de6c9604d781dca8e3fdf2f0238d7c062d5dbfa2
Yasin Beken <yasinbeken16@gmail.com>
2024-03-04 12:32:36 +0300

    First Commit

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] private AudioClip clickSound;
+    int count=0;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Collectible"))
+        {
+            Debug.Log(count);
+            count++;
+            AudioSource.PlayClipAtPoint(clickSound, other.transform.position);
+            Destroy(other.gameObject);
+        }
+    }
+}
